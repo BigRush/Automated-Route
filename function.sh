fd043061518622a8d5abad94e3442834b1c27ee4
BigRush <BigRush@Rig.com>
2018-05-05 13:31:03 +0300

    base structure

---
+++ function.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env #!/usr/bin/env bash
+
+#######################################################################################
+# Authors	: chn555 & BigRush
+#
+#License	: GPLv3
+#
+# Description	: DNS & DHCP server installation and configuration
+#
+# Version	: 1.0.0
+#######################################################################################
+
+
+####Functions####
+
+Root_Check () {		## checks that the script runs as root
+	if [[ $EUID -eq 0 ]]; then
+		:
+	else
+		printf "$line\n"
+		printf "The script needs to run with root privileges\n"
+		printf "$line\n"
+		exit 1
+	fi
+}
+
+Distro_Check () {		## checking the environment the user is currenttly running on to determine which settings should be applied
+	cat /etc/*-release |grep ID |cut  -d "=" -f "2" |egrep "^arch$|^manjaro$" &> /dev/null
+
+	if [[ $? -eq 0 ]]; then
+	  	Distro_Val="arch"
+	else
+	  	:
+	fi
+
+  cat /etc/*-release |grep ID |cut  -d "=" -f "2" |egrep "^debian$|^\"Ubuntu\"$" &> /dev/null
+
+  if [[ $? -eq 0 ]]; then
+    	Distro_Val="debian"
+  else
+    	:
+  fi
+
+	cat /etc/*-release |grep ID |cut  -d "=" -f "2" |egrep "^\"centos\"$|^\"fedora\"$" &> /dev/null
+
+	if [[ $? -eq 0 ]]; then
+	   	Distro_Val="centos"
+	else
+		:
+	fi
+}
+
+DHCP_Installtion () {
+
+}
+
+
+DHCP_Configuration () {
+
+}
+
+
+DNS_Installation () {
+
+}
+
+DNS_Configuration () {
+
+}
